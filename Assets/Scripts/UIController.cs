f6794c54a882b7bec600a8bc9ab027a1bb672080
Erick Simões <erick.simoes@live.com>
2019-06-07 19:48:02 -0300

    Add UIController

---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIController : MonoBehaviour {
+
+	[SerializeField]
+	public static List<string> sceneStack;
+
+	void Start() {
+		if (sceneStack == null) {
+			print("instanciado");
+			sceneStack = new List<string>();
+		}
+	}
+
+
+	public void GoToScene(string scene) {
+		sceneStack.Add(SceneManager.GetActiveScene().name);
+		SceneManager.LoadScene(scene);
+	}
+
+	public void BackToPreviousScene() {
+		string previousScene = sceneStack[sceneStack.Count - 1];
+		sceneStack.RemoveAt(sceneStack.Count - 1);
+		SceneManager.LoadScene(previousScene);
+	}
+}
