b111f42d5447968cd928f783190e3156392a2b58
Erick Simões <erick.simoes@live.com>
2019-06-09 18:04:57 -0300

    Add EventTrackerController

---
+++ Assets/Scripts/EventTrackerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class EventTrackerController : MonoBehaviour, ITrackableEventHandler {
+	
+	private TrackableBehaviour trackableBehaviour;
+
+	void Start() {
+		trackableBehaviour = GetComponent<TrackableBehaviour>();
+		if (trackableBehaviour) {
+			trackableBehaviour.RegisterTrackableEventHandler(this);
+		}
+	}
+
+	void ITrackableEventHandler.OnTrackableStateChanged(TrackableBehaviour.Status previousStatus, TrackableBehaviour.Status newStatus) {
+		if (newStatus == TrackableBehaviour.Status.DETECTED || newStatus == TrackableBehaviour.Status.TRACKED || newStatus == TrackableBehaviour.Status.EXTENDED_TRACKED) {
+			Debug.Log("Target Detected");
+		} else {
+			Debug.Log("Targed lost");
+		}
+	}
+
+}
