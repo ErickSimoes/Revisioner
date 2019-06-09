abe47beafc7f9a6a3be1f34f138fb2655a4cf3eb
Erick Simões <erick.simoes@live.com>
2019-06-09 20:29:46 -0300

    Remove unused event

--- Assets/Scripts/EventTrackerController.cs
+++ Assets/Scripts/EventTrackerController.cs
@@ -2,11 +2,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Vuforia;
 
-public class EventTrackerController : MonoBehaviour, ITrackableEventHandler {
+public class EventTrackerController : MonoBehaviour {
 	
-	private TrackableBehaviour trackableBehaviour;
 	private static GameObject Subject;
 	private UIController.Subjects subject;
 	[NonSerialized]
@@ -25,11 +23,6 @@ public class EventTrackerController : MonoBehaviour, ITrackableEventHandler {
 	}
 
 	void Start() {
-		trackableBehaviour = GetComponent<TrackableBehaviour>();
-		if (trackableBehaviour) {
-			trackableBehaviour.RegisterTrackableEventHandler(this);
-		}
-
 		subject = Subject.GetComponent<ARSubjectController>().subject;
 
 		foreach (Prefabs prefab in prefabs) {
@@ -38,14 +31,6 @@ public class EventTrackerController : MonoBehaviour, ITrackableEventHandler {
 			}
 		}
 		
-	}
-
-	void ITrackableEventHandler.OnTrackableStateChanged(TrackableBehaviour.Status previousStatus, TrackableBehaviour.Status newStatus) {
-		if (newStatus == TrackableBehaviour.Status.DETECTED || newStatus == TrackableBehaviour.Status.TRACKED || newStatus == TrackableBehaviour.Status.EXTENDED_TRACKED) {
-			Debug.Log("Target Detected");
-		} else {
-			Debug.Log("Targed lost");
-		}
 	}
 
 }
