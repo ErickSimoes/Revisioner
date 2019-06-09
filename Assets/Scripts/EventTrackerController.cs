8b6348fac1b23a5bfc06a8c0fe17374cc1c3d654
Erick Simões <erick.simoes@live.com>
2019-06-09 19:20:20 -0300

    Implementation get subject

--- Assets/Scripts/EventTrackerController.cs
+++ Assets/Scripts/EventTrackerController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Vuforia;
@@ -6,12 +7,31 @@ using Vuforia;
 public class EventTrackerController : MonoBehaviour, ITrackableEventHandler {
 	
 	private TrackableBehaviour trackableBehaviour;
+	private static GameObject Subject;
+	private UIController.Subjects subject;
+	[NonSerialized]
+	public int numero;
+	[Serializable]
+	public struct Prefabs {
+		public UIController.Subjects subject;
+		public GameObject prefab;
+	}
+	public Prefabs[] prefabs;
+
+	void Awake() {
+		if (!Subject) {
+			Subject = GameObject.FindGameObjectWithTag("Subject");
+		}
+	}
 
 	void Start() {
 		trackableBehaviour = GetComponent<TrackableBehaviour>();
 		if (trackableBehaviour) {
 			trackableBehaviour.RegisterTrackableEventHandler(this);
 		}
+
+		subject = Subject.GetComponent<ARSubjectController>().subject;
+		
 	}
 
 	void ITrackableEventHandler.OnTrackableStateChanged(TrackableBehaviour.Status previousStatus, TrackableBehaviour.Status newStatus) {
